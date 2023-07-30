cabce9a09fb58f8767f7ddbbaeffd2e17bf8833d
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2023-07-30 20:11:49 -0300

    add app_colors.dart

---
+++ roll_dice/lib/shared/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color selectiveYellow = Color(0xFFFCBA04);
+  static const Color salmonPink = Color(0xFFEF9A9A);
+  static const Color turkeyRed = Color(0xFFA50104);
+  static const Color roseWood = Color(0xFF590004);
+  static const Color blackBean = Color(0xFF250001);
+  static const List<Color> backgroundGradient = [
+    AppColors.selectiveYellow,
+    AppColors.salmonPink,
+    AppColors.turkeyRed,
+    AppColors.roseWood,
+    AppColors.blackBean,
+  ];
+}
