39bce3f595962c626f101d1d8f8ef36be38c49d8
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2023-08-02 20:56:19 -0300

    add quiz_app.dart

---
+++ quiz/lib/src/quiz_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/src/shared/app_colors.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Quiz",
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.fernGreen),
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
