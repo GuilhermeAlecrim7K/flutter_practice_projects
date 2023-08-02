3fbc791182c0640bcd4182bb8613b6cb05ccf54b
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2023-08-02 20:53:36 -0300

    add app_colors.dart

---
+++ quiz/lib/src/shared/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color fernGreen = Color(0xFF537A5A);
+  static const Color celadon = Color(0xFF9ae19d);
+  static const Color battleshipGrey = Color(0xFF909590);
+  static const Color outerSpace = Color(0xFF474a48);
+  static const Color jet = Color(0xFF2c302e);
+}
