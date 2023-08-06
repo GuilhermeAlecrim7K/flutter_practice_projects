7237a94e2d852e4ec56c9a4a901069ece39ac0a2
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2023-08-06 17:05:56 -0300

    add font license to license registry

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quiz/src/quiz_app.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle
+        .loadString('google_fonts/roboto_condensed/LICENSE.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const QuizApp());
 }
